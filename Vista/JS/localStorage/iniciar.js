1aaa89f310ac0d2f792c714e1cb4bce535dd656c
Yarce <camiloyarce11@gmail.com>
2023-11-27 18:09:49 -0500

    nuevas alertas (en inicio de sesión y registro)

--- Vista/JS/localStorage/iniciar.js
+++ Vista/JS/localStorage/iniciar.js
@@ -1,24 +1,63 @@
 let btnIniciar = document.getElementById('ingresar');
 btnIniciar.addEventListener('click', function iniciar(evento) {
     evento.preventDefault();
-    let email = document.getElementById("email").value;
-    let pass = document.getElementById("password").value;
-    let listaUsuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+    if (validacion()) {
+        let email = document.getElementById("email").value;  // Corrected ID
+        let pass = document.getElementById("password").value;
+        let listaUsuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
 
-    let encontrado = listaUsuarios.some(function (usuario) {
-        return usuario.email === email && usuario.password === pass;
-    });
+        let encontrado = listaUsuarios.some(function (usuario) {
+            return usuario.email === email && usuario.password === pass;
+        });
 
-    if (encontrado) {
-        //Poner sweetAlert
-        setTimeout(function () {
-            window.location.href = "bienvenido.html";
-        }, 1);
-    } else {
-        alert("Usuario no encontrado. Verifique sus credenciales e intente de nuevo.");
+        if (encontrado) {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 2000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.onmouseenter = Swal.stopTimer;
+                    toast.onmouseleave = Swal.resumeTimer;
+                }
+            });
+            Toast.fire({
+                icon: "success",
+                title: "Usuario registrado \n¡Por favor iniciar sesión!"
+            });
+            setTimeout(function () {
+                window.location.href = "Bienvenido.html";
+            }, 2001);
+        } else {
+            alert("Usuario no encontrado. Verifique sus credenciales e intente de nuevo.");
+        }
     }
-})
+});
 
-function iniciar() {
+function validacion() {
+    const correo = document.getElementById("email").value;  // Corrected ID
+    const password = document.getElementById("password").value;
 
+    if (correo == "" || password == "") {
+        event.preventDefault();  // Corrected to event.preventDefault();
+        const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 2000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+            }
+        });
+        Toast.fire({
+            icon: "error",
+            title: "Usuario no encontrado \n¡Por favor verifica tus credenciales!"
+        });
+        return false;
+    } else {
+        return true;
+    }
 }
--- Vista/JS/localStorage/registrar.js
+++ Vista/JS/localStorage/registrar.js
@@ -1,19 +1,115 @@
 let btnRegister = document.getElementById('registro');
 btnRegister.addEventListener('click', function registro(event) {
     event.preventDefault();
+    if (validacion()) {
+        let listaUsuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+        let id
+        listaUsuarios.length != 0 ? listaUsuarios.findLast((usuario) => id = usuario.id) : id = 0
 
-    let listaUsuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
-    let id = listaUsuarios[listaUsuarios.length]
-
-    let nuevoUsuario = {
-        id: id + 1,
-        nombre: document.getElementById('nombre').value,
-        apellido: document.getElementById('apellido').value,
-        celular: document.getElementById('celular').value,
-        email: document.getElementById('email').value,
-        password: document.getElementById('password').value
+        let nuevoUsuario = {
+            id: id + 1,
+            nombre: document.getElementById('nombre').value,
+            apellido: document.getElementById('apellido').value,
+            celular: document.getElementById('celular').value,
+            email: document.getElementById('email').value,
+            password: document.getElementById('password').value
+        }
+        listaUsuarios.push(nuevoUsuario);
+        localStorage.setItem('usuarios', JSON.stringify(listaUsuarios));
+        alerta();
+        document.getElementById('form').reset();
     }
-    listaUsuarios.push(nuevoUsuario);
-    localStorage.setItem('usuarios', JSON.stringify(listaUsuarios));
-    document.getElementById('form').reset();
 });
+function validacion() {
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let celular = document.getElementById("celular").value;
+    let correo = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    if (nombre == "" || apellido == "" || celular == "" || correo == "" || password == "") {
+        event.preventDefault();
+        const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 2000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+            }
+
+        });
+        Toast.fire({
+            icon: "error",
+            title: "¡Asegurate de llenar todos los campos!"
+        });
+        return false
+    } else {
+        const example_correo = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
+        let confirmacion_e = example_correo.test(correo);
+        if (confirmacion_e == false) {
+            event.preventDefault();
+            const Toast = Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 2000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.onmouseenter = Swal.stopTimer;
+                    toast.onmouseleave = Swal.resumeTimer;
+                }
+            });
+            Toast.fire({
+                icon: "error",
+                title: "¡Correo electrónico no válido!"
+            });
+            return false
+        } else {
+            const example_pass = /^.{4,30}$/;
+            let confirmacion_pass = example_pass.test(password);
+            if (confirmacion_pass == false) {
+                event.preventDefault();
+                const Toast = Swal.mixin({
+                    toast: true,
+                    position: "top-end",
+                    showConfirmButton: false,
+                    timer: 2000,
+                    timerProgressBar: true,
+                    didOpen: (toast) => {
+                        toast.onmouseenter = Swal.stopTimer;
+                        toast.onmouseleave = Swal.resumeTimer;
+                    }
+                });
+                Toast.fire({
+                    icon: "error",
+                    title: "¡El límite de caracteres es de 4 a 30, intente de nuevo!"
+                });
+                return false
+            }
+        }
+    }
+    return true
+}
+function alerta() {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 2000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.onmouseenter = Swal.stopTimer;
+            toast.onmouseleave = Swal.resumeTimer;
+        }
+    });
+    Toast.fire({
+        icon: "success",
+        title: "Usuario registrado \n¡Por favor iniciar sesión!"
+    });
+    setTimeout(function () {
+        window.location.href = "InicioSesion.html";
+    }, 2001);
+}
