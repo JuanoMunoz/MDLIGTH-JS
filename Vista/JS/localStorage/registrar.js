e2d5c42b39f031d263ef2098d1e8aa0051711cfd
Yarce <camiloyarce11@gmail.com>
2023-11-27 16:43:19 -0500

    nada

--- Vista/JS/localStorage/registrar.js
+++ Vista/JS/localStorage/registrar.js
@@ -4,31 +4,16 @@ btnRegister.addEventListener('click', function registro(event) {
 
     let listaUsuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
     let id = listaUsuarios.length > 0 ? listaUsuarios[listaUsuarios.length - 1].id : 0;
-
-    if (document.getElementById('id').value) {
-        let usuario = listaUsuarios.find(u => u.id === parseInt(document.getElementById('id').value));
-        if (usuario) {
-            usuario.nombre = document.getElementById('nombre').value;
-            usuario.apellido = document.getElementById('apellido').value;
-            usuario.celular = document.getElementById('celular').value;
-            usuario.email = document.getElementById('email').value;
-            usuario.password = document.getElementById('password').value;
-        }
-        document.getElementById('id').value = "";
-    } else {
-        let nuevoUsuario = {
-            id: id + 1,
-            nombre: document.getElementById('nombre').value,
-            apellido: document.getElementById('apellido').value,
-            celular: document.getElementById('celular').value,
-            email: document.getElementById('email').value,
-            password: document.getElementById('password').value
-        }
-        listaUsuarios.push(nuevoUsuario);
+    let nuevoUsuario = {
+        id: id + 1,
+        nombre: document.getElementById('nombre').value,
+        apellido: document.getElementById('apellido').value,
+        celular: document.getElementById('celular').value,
+        email: document.getElementById('email').value,
+        password: document.getElementById('password').value
     }
+    listaUsuarios.push(nuevoUsuario);
 
     localStorage.setItem('usuarios', JSON.stringify(listaUsuarios));
-    alert("Funcionó");
-    console.log(localStorage.getItem('usuarios'));
     document.getElementById('form').reset();
 });
--- Vista/JS/sesion.js
+++ Vista/JS/sesion.js
@@ -5,28 +5,16 @@ const boton = document.getElementById("ingresar");
 
 boton.addEventListener("click", validar);
 
-function validar(e){
+function validar(e) {
     const correo = document.getElementById("correo").value;
     const password = document.getElementById("password").value;
-
     //CORREO
-    if(correo == ""){
+    if (correo == "" || password == "") {
         e.preventDefault();
-
         Swal.fire({
             icon: 'error',
             title: 'Oops...',
-            text: 'El campo Correo no puede ir vacio!',
-        })
-    }
-    //CONTRASEÑA
-    if(password == ""){
-        e.preventDefault();
-
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'El campo Contraseña no puede ir vacio!',
+            text: 'Asegurate de llenar todos los campos!',
         })
     }
 }
